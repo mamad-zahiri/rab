4bd4badd046e84bf10deffac98c13f62b5eaf1b5
Mohamad Zahiri <mamadzhri@gamil.com>
2024-04-04 12:47:49 +0330

    feat: prepare temp, take picture, analyze, set brightness

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+mod config;
+mod fs;
+mod image;
+mod utils;
+
+use std::thread::sleep;
+use std::time::Duration;
+
+use utils::set_brightness;
+
+use crate::config::MY_TMP;
+use crate::fs::{cd, clear_dir, mk_dir};
+use crate::image as img;
+
+fn setup_my_tmp() {
+    // Setup my tmp working directory
+    mk_dir(MY_TMP);
+
+    // Move to working directory
+    cd(MY_TMP);
+
+    // Clear working directory
+    clear_dir(MY_TMP);
+}
+
 fn main() {
-    println!("Hello, world!");
+    setup_my_tmp();
+
+    loop {
+        // Take a picture
+        match img::take() {
+            Err(err) => panic!("take image: error: {:?}", err),
+            Ok(_) => {}
+        }
+
+        let img = img::read("00000001.jpg");
+        clear_dir(MY_TMP);
+        let brightness = img::analyze(&img.unwrap());
+
+        set_brightness(brightness);
+
+        sleep(Duration::from_secs(utils::config().delay as u64));
+    }
 }
